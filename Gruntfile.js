5823a1a492b49460686fda9836125479e60151ab
Mike Frysinger <vapier@gmail.com>
2025-02-02 01:12:15 -0500

    merge background scripts into one page
    Since MV3 only supports loading one script directly, merge them into one.
    This is easier than rewriting everything into ES6 modules.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,23 @@ module.exports = function(grunt) {
     grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
       manifest: grunt.file.readJSON('src/manifest.json'),
+      concat: {
+        options: {},
+        dist: {
+          src: [
+            "src/js/db.js", "src/js/storage.js", "src/js/tabStates.js", "src/js/eventPage.js",
+          ],
+          dest: "src/js/background.js",
+        },
+      },
       crx: {
         myPublicExtension: {
           src: [
               "src/**/*",
+              "!src/js/db.js",
+              "!src/js/storage.js",
+              "!src/js/tabStates.js",
+              "!src/js/eventPage.js",
               "!**/screenshot*.png",
               "!**/Thumbs.db"
           ],
@@ -27,7 +40,8 @@ module.exports = function(grunt) {
       }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-crx');
-    grunt.registerTask('default', ['crx']);
+    grunt.registerTask('default', ['concat', 'crx']);
 };
 
